1df1aa90ef92abbd9c42a82d8bd11f42f8910125
mayrant <mayrant2004@yahoo.com>
2021-10-07 22:17:25 -0500

    pie chart and total complete

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,9 +8,8 @@ import {
     TableRow
 } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
-import Chart from './Chart'
-import Total from './Total'
-// import AddCar from './AddCar'
+import Chart from '../containers/Chart'
+import Total from '../containers/Total'
 import AddCar from '../containers/AddCar'
  
 const Dashboard = (props) => {
